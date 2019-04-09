38d04ccb80031c2ec6151822b66b563239787980
Anastasia Belenkii <anastasia@belenky.net>
2019-04-09 00:20:11 -0700

    done with part 1

--- skeleton/part1/main.go
+++ skeleton/part1/main.go
@@ -19,11 +19,45 @@ type Message struct {
 }
 
 func main() {
-	// TODO: Create a new bufio.Scanner reading from the standard input.
-	// TODO: Create a new json.Encoder writing into the standard output.
-	for /* TODO: Iterate over every line in the scanner */ {
-		// TODO: Create a new message with the read text.
-		// TODO: Encode the message, and check for errors!
+	//TODO: Create a new bufio.Scanner reading from the standard input.
+	enc := json.NewEncoder(os.Stdout)
+	//TODO: Create a new json.Encoder writing into the standard output.
+	scanner := bufio.NewScanner(os.Stdin)
+	var msgs []Message
+
+	//TODO: Iterate over every line in the scanner
+	for scanner.Scan() && (scanner.Text() != "q") {
+		//TODO: Create a new message with the read text.
+		var msg Message
+		msg.Body = scanner.Text()
+		msgs = append(msgs, msg)
 	}
-	// TODO: Check for a scan error.
+	//TODO: Check for a scan error.
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	//TODO: Encode the message, and check for errors!
+	for _, s := range msgs {
+		err := enc.Encode(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
+
+/******SOLUTION:
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	e := json.NewEncoder(os.Stdout)
+	for s.Scan() {
+		m := Message{Body: s.Text()}
+		err := e.Encode(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+*/
